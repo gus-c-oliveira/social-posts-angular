d596784c687d6417af6da7c714b65607a0b8baf6
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-07 22:00:49 -0300

    router: create app routes and routing module

---
+++ src/app/router/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { APP_ROUTES } from './app-routes';
+
+export const AppBaseHref = { provide: APP_BASE_HREF, useValue: '/' };
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  providers: [AppBaseHref],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
