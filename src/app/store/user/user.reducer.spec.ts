9f9de67dc145ee6d3d3605489ba6bfa48ae47e21
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-09 11:06:35 -0300

    user-store: update reducer with new User Actions

--- src/app/store/user/user.reducer.spec.ts
+++ src/app/store/user/user.reducer.spec.ts
@@ -1,9 +1,44 @@
+import { mockUserList } from '@app/mocks';
+
+import { LoadUsers, LoadUsersError, LoadUsersSuccess } from './user.actions';
 import { userReducer } from './user.reducer';
 import { initialUserState } from './user.state';
 
 describe('UserReducer', () => {
-  it('should return the user initial state', () => {
-    const returnedState = userReducer(null, null);
-    expect(returnedState).toEqual(initialUserState);
+  describe('LoadUsers', () => {
+    it('should set loading to true and error to false', () => {
+      const newState = userReducer(initialUserState, new LoadUsers());
+      expect(newState).toEqual({
+        ...initialUserState,
+        loading: true,
+        error: false,
+      });
+    });
+  });
+
+  describe('LoadUsersSuccess', () => {
+    it('should set users to received data and loading to false', () => {
+      const newState = userReducer(
+        initialUserState,
+        new LoadUsersSuccess(mockUserList)
+      );
+      expect(newState).toEqual({
+        ...initialUserState,
+        loading: false,
+        users: mockUserList,
+        error: false,
+      });
+    });
+  });
+
+  describe('LoadUsersError', () => {
+    it('should set loading to false and error to true', () => {
+      const newState = userReducer(initialUserState, new LoadUsersError());
+      expect(newState).toEqual({
+        ...initialUserState,
+        loading: false,
+        error: true,
+      });
+    });
   });
 });
