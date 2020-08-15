7f37fd676c9849f58b3581f279e96f12c894c643
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-15 15:20:16 -0300

    router: import routes from user folder

--- src/app/router/app-routes.ts
+++ src/app/router/app-routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import {
+  USER_LIST_PATH,
+  USER_POST_PATH,
+  USER_PROFILE_PATH,
   UserListComponent,
   UserPostComponent,
   UserProfileComponent,
 } from '@app/user';
 
 export const APP_ROUTES: Routes = [
-  { path: 'user-list', component: UserListComponent },
-  { path: 'user-post', component: UserPostComponent },
-  { path: 'user-profile', component: UserProfileComponent },
+  { path: USER_LIST_PATH, component: UserListComponent },
+  { path: USER_POST_PATH, component: UserPostComponent },
+  { path: USER_PROFILE_PATH, component: UserProfileComponent },
   { path: '**', component: UserListComponent },
 ];
