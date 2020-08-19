46ca9158959dd64c8774dd89e1cb802006ba19bc
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-19 18:58:00 -0300

    post-store: add Post store to App State

--- src/app/store/app/app.state.ts
+++ src/app/store/app/app.state.ts
@@ -1,14 +1,17 @@
 import { RouterReducerState } from '@ngrx/router-store';
 
 import { initialUserState, USER_STATE_KEY, UserState } from '../user';
+import { initialPostState, POST_STATE_KEY, PostState } from '../post';
 
 export interface AppState {
   router?: RouterReducerState;
   [USER_STATE_KEY]: UserState;
+  [POST_STATE_KEY]: PostState;
 }
 
-export const initialAppState = {
+export const initialAppState: AppState = {
   [USER_STATE_KEY]: initialUserState,
+  [POST_STATE_KEY]: initialPostState,
 };
 
 export const getInitialAppState = (): AppState => initialAppState;
---
+++ src/app/store/post/index.ts
@@ -0,0 +1,4 @@
+export * from './post.model';
+export * from './post.state';
+export * from './post.reducer';
+export * from './post.actions';
