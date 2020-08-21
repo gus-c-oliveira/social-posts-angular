2f4b88d5b5417dcfd31d45aede248b72285ce139
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-21 20:30:39 -0300

    user-profile: display user's posts in profile

--- src/app/components/user/profile/user-profile.component.ts
+++ src/app/components/user/profile/user-profile.component.ts
@@ -1,8 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
-import { AppState, User, userQuery } from '@app/store';
+import {
+  AppState,
+  User,
+  userQuery,
+  LoadPosts,
+  Post,
+  postQuery,
+} from '@app/store';
 import { select, Store } from '@ngrx/store';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { Observable } from 'rxjs';
+import { tap, filter } from 'rxjs/operators';
 
 export const userProfileSelector = 'app-user-profile';
 export const USER_PROFILE_PATH = 'user-profile';
@@ -15,11 +23,19 @@ export const USER_PROFILE_PATH = 'user-profile';
 })
 export class UserProfileComponent implements OnDestroy {
   public user$: Observable<User>;
-  public labels: string[];
+  public posts$: Observable<Post[]>;
 
   public constructor(private store: Store<AppState>) {
+    // Get selected user from store
     this.user$ = this.store.pipe(
       select(userQuery.getSelectedUser),
+      filter((user) => !!user),
+      // Dispatch action to load user posts
+      tap((user) => this.store.dispatch(new LoadPosts(user.id))),
+      untilDestroyed(this)
+    );
+    this.posts$ = this.store.pipe(
+      select(postQuery.getPosts),
       untilDestroyed(this)
     );
   }
