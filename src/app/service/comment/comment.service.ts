940f22cd9b4659baea93bfce105a3bfb7cf46ac6
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-08-22 11:24:12 -0300

    service: create comment service

---
+++ src/app/service/comment/comment.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Comment } from '@app/store';
+import { Observable } from 'rxjs';
+
+import { APP_CONSTANTS } from '../../app.constants';
+
+@Injectable()
+export class CommentService {
+  private baseURL = APP_CONSTANTS.baseURL;
+
+  public constructor(private http: HttpClient) {}
+
+  public getComments(postId: number): Observable<Comment[]> {
+    const url = this.baseURL + `comments?postId=${postId}`;
+    return this.http.get<Comment[]>(url);
+  }
+}
