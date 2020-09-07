5efcb8964b1e6a3b10a4b6e43cd02b818bc73f45
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-09-06 21:30:08 -0300

    header: create header component

---
+++ src/app/components/ui/header/header.component.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { APP_CONSTANTS } from 'src/app/app.constants';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let fixture: ComponentFixture<HeaderComponent>;
+  let component: HeaderComponent;
+  const buttonText = 'Click me!';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    component.title = APP_CONSTANTS.AppTitle;
+    component.buttonText = buttonText;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the title', () => {
+    const title = fixture.debugElement
+      .query(By.css('h1'))
+      .nativeElement.textContent.trim();
+    expect(title).toEqual(APP_CONSTANTS.AppTitle);
+  });
+
+  it('should display the button', () => {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    expect(button).toBeTruthy();
+  });
+
+  it('should display the button text inside the button', () => {
+    const text = fixture.debugElement
+      .query(By.css('button'))
+      .nativeElement.textContent.trim();
+    expect(text).toEqual(buttonText);
+  });
+
+  it('should emit an event when the button is clicked', () => {
+    spyOn(component.buttonClick, 'emit');
+    fixture.debugElement.query(By.css('button')).nativeElement.click();
+    expect(component.buttonClick.emit).toHaveBeenCalled();
+  });
+});
---
+++ src/app/components/ui/header/header.component.ts
@@ -0,0 +1,25 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+export const headerSelector = 'app-header';
+
+@Component({
+  selector: headerSelector,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+  @Input() public title = '';
+  @Input() public buttonText = '';
+  @Output() public buttonClick = new EventEmitter<void>();
+
+  public emitButtonClick() {
+    this.buttonClick.emit();
+  }
+}
