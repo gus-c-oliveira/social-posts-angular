c8b376f009e379ccbb00d103d0dc02a63e740e7b
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-09-15 08:43:54 -0300

    App: scroll to top after navigating between pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { USER_LIST_PATH } from '@app/user';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { filter } from 'rxjs/operators';
 
 import { APP_CONSTANTS } from './app.constants';
 
@@ -9,15 +11,30 @@ import { APP_CONSTANTS } from './app.constants';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   public title = APP_CONSTANTS.AppTitle;
   public buttonText = 'User List';
 
-  public constructor(private router: Router, private route: ActivatedRoute) {}
+  public constructor(private router: Router, private route: ActivatedRoute) {
+    this.setupScrollToTopOnNavigation();
+  }
+
+  private setupScrollToTopOnNavigation() {
+    this.router.events
+      .pipe(
+        untilDestroyed(this),
+        filter((event) => event instanceof NavigationEnd)
+      )
+      .subscribe((event) => {
+        window.scrollTo(0, 0);
+      });
+  }
 
   public navigateToUserList() {
     this.router.navigate([USER_LIST_PATH], {
       relativeTo: this.route.parent,
     });
   }
+
+  public ngOnDestroy() {}
 }
