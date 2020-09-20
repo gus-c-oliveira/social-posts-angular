91cf9d0b1281204d0370f36ec75e272875b7001c
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-09-20 10:30:42 -0300

    language-selectore: rename component selector

--- src/app/components/ui/language-selector/language-selector.component.ts
+++ src/app/components/ui/language-selector/language-selector.component.ts
@@ -2,10 +2,10 @@ import { Component, OnDestroy } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 
-export const selector = 'app-language-selector';
+export const languageComponentSelector = 'app-language-selector';
 
 @Component({
-  selector,
+  selector: languageComponentSelector,
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss'],
 })
