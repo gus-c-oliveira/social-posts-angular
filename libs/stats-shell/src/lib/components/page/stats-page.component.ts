001e495c13bdb37b8f4cdbbe44abdda1ee15b3e8
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-04 20:59:51 -0300

    stats-shell: create stats-page component

---
+++ libs/stats-shell/src/lib/components/page/stats-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+export const selector = 'gus-stats-page';
+
+@Component({
+  selector,
+  templateUrl: './stats-page.component.html',
+  styleUrls: ['./stats-page.component.scss'],
+})
+export class StatsPageComponent {}
--- libs/stats-shell/src/lib/stats-shell.module.ts
+++ libs/stats-shell/src/lib/stats-shell.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { StatsPageComponent } from './components/index';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [StatsPageComponent],
+  exports: [StatsPageComponent],
 })
 export class StatsShellModule {}
