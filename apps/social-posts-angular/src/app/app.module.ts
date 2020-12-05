f9dbfe5c77fd3cde889ce1066875313a0535b8ae
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-05 14:27:10 -0300

    app: remove user translations

--- apps/social-posts-angular/src/app/app.module.ts
+++ apps/social-posts-angular/src/app/app.module.ts
@@ -45,6 +45,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient],
       },
+      isolate: true,
     }),
   ],
   providers: [
