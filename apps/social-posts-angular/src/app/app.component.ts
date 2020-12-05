3c97d97da5c6291f5b7ed3ea91ac3599c550f933
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-05 10:32:13 -0300

    app: navigate to areas using header buttons

--- apps/social-posts-angular/src/app/app.component.ts
+++ apps/social-posts-angular/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { HeaderButtonConfig } from '@gus/ui';
 import { TranslateService } from '@ngx-translate/core';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { filter } from 'rxjs/operators';
+
+import { STATS_AREA_PATH, USER_AREA_PATH } from './router/app.routes';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +13,26 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
+  public title = 'HEADER.APP_TITLE';
+  public headerButtonConfigs: HeaderButtonConfig[] = [
+    {
+      text: 'HEADER.BUTTON.USER',
+      eventType: 'UserButtonClick',
+    },
+    {
+      text: 'HEADER.BUTTON.STATS',
+      eventType: 'StatsButtonClick',
+    },
+  ];
+
   public constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private translateService: TranslateService
   ) {
     this.addTranslations();
     this.setupDefaultLanguage();
-    this.setupScrollToTopOnNavigation();
+    this.listenToNavigationEndEvents();
   }
 
   private addTranslations() {
@@ -32,7 +48,7 @@ export class AppComponent implements OnDestroy {
     }
   }
 
-  private setupScrollToTopOnNavigation() {
+  private listenToNavigationEndEvents() {
     this.router.events
       .pipe(
         untilDestroyed(this),
@@ -41,6 +57,24 @@ export class AppComponent implements OnDestroy {
       .subscribe((event) => {
         window.scrollTo(0, 0);
       });
+  }
+
+  public reduce(event) {
+    this[`reduce${event.type}`]
+      ? this[`reduce${event.type}`]()
+      : console.warn('Event not handled!', event);
+  }
+
+  private reduceStatsButtonClick() {
+    this.router.navigate([STATS_AREA_PATH], {
+      relativeTo: this.route.parent,
+    });
+  }
+
+  private reduceUserButtonClick() {
+    this.router.navigate([USER_AREA_PATH], {
+      relativeTo: this.route.parent,
+    });
   }
 
   public ngOnDestroy() {}
