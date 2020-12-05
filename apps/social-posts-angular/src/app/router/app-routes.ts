f989877689c52187388ec3669e6891b90d9233c2
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-04 21:22:03 -0300

    app: add route for stats-shell

--- apps/social-posts-angular/src/app/router/app-routes.ts
+++ apps/social-posts-angular/src/app/router/app-routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
   { path: 'user', loadChildren: '@gus/user-shell#UserShellModule' },
+  { path: 'stats', loadChildren: '@gus/stats-shell#StatsShellModule' },
   { path: '**', redirectTo: 'user' },
 ];
