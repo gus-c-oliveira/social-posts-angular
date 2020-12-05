2abeab838f1a8465511a43d6adf9a04fd14209ed
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-05 10:21:58 -0300

    user-shell: remove header from user-page

--- libs/user-shell/src/lib/components/page/page.component.ts
+++ libs/user-shell/src/lib/components/page/page.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { USER_LIST_PATH } from '../list/index';
 
 export const pageSelector = 'gus-user-page';
 
@@ -9,15 +7,4 @@ export const pageSelector = 'gus-user-page';
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
 })
-export class UserPageComponent {
-  public title = 'HEADER.APP_TITLE';
-  public buttonText = 'HEADER.BUTTON';
-
-  public constructor(private router: Router, private route: ActivatedRoute) {}
-
-  public navigateToUserList() {
-    this.router.navigate([USER_LIST_PATH], {
-      relativeTo: this.route.parent,
-    });
-  }
-}
+export class UserPageComponent {}
