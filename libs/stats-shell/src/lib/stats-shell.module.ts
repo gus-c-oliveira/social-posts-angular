eab9e993c216964de7f017f8958f150721d1bb87
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-05 09:21:54 -0300

    stats-shell: add translation for warning

--- libs/stats-shell/src/lib/stats-shell.module.ts
+++ libs/stats-shell/src/lib/stats-shell.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { StatsPageComponent } from './components/index';
 import { ROUTES } from './routes/index';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  imports: [CommonModule, RouterModule.forChild(ROUTES), TranslateModule],
   declarations: [StatsPageComponent],
   exports: [StatsPageComponent],
 })
