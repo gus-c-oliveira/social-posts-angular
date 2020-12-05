8aed5874cdfa451e8f1f8fe81d2dcef2b5fac325
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-04 21:09:07 -0300

    stats-shell: create route for stats-page

--- libs/stats-shell/src/lib/stats-shell.module.ts
+++ libs/stats-shell/src/lib/stats-shell.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { StatsPageComponent } from './components/index';
+import { ROUTES } from './routes/index';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(ROUTES)],
   declarations: [StatsPageComponent],
   exports: [StatsPageComponent],
 })
