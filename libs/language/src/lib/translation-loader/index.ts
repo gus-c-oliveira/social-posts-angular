2fcefc0379f0352f73456345e79826979440348e
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-06 13:44:29 -0300

    language: create files for model and token

--- libs/language/src/lib/translation-loader/index.ts
+++ libs/language/src/lib/translation-loader/index.ts
@@ -1,1 +1,3 @@
+export * from './token';
 export * from './translation-loader';
+export * from './translations.model';
--- libs/language/src/lib/translation-loader/translation-loader.ts
+++ libs/language/src/lib/translation-loader/translation-loader.ts
@@ -1,15 +1,9 @@
-import { Inject, InjectionToken } from '@angular/core';
+import { Inject } from '@angular/core';
 import { TranslateLoader } from '@ngx-translate/core';
 import { from, Observable } from 'rxjs';
 
-export interface Translations {
-  'en-US': any;
-  'pt-BR': any;
-}
-
-export const TRANSLATIONS_TOKEN = new InjectionToken<Translations>(
-  'Object containing translations'
-);
+import { TRANSLATIONS_TOKEN } from './token';
+import { Translations } from './translations.model';
 
 export class LibTranslationLoader implements TranslateLoader {
   public constructor(
