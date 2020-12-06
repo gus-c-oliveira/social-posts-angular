a2160573c0eeb6629b97592b9e5d0701401e5177
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-12-05 21:15:27 -0300

    stats-shell: load translations using LibTranslationLoader

--- libs/stats-shell/src/lib/stats-shell.module.ts
+++ libs/stats-shell/src/lib/stats-shell.module.ts
@@ -1,29 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LanguageModule } from '@gus/language';
+import {
+  LanguageModule,
+  LibTranslationLoaderFactory,
+  TRANSLATIONS_TOKEN,
+} from '@gus/language';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import en from '../assets/i18n/en-US.json';
 import pt from '../assets/i18n/pt-BR.json';
 import { StatsPageComponent } from './components/index';
 import { ROUTES } from './routes/index';
-import { from, Observable } from 'rxjs';
-
-class LibTranslationLoader implements TranslateLoader {
-  private locales = {
-    'en-US': en,
-    'pt-BR': pt,
-  };
-
-  public getTranslation(lang: string): Observable<any> {
-    return from([this.locales[lang] || this.locales['en-US']]);
-  }
-}
-
-function LibTranslationLoaderFactory() {
-  return new LibTranslationLoader();
-}
 
 @NgModule({
   imports: [
@@ -33,6 +21,7 @@ function LibTranslationLoaderFactory() {
       loader: {
         provide: TranslateLoader,
         useFactory: LibTranslationLoaderFactory,
+        deps: [TRANSLATIONS_TOKEN],
       },
       isolate: true,
     }),
@@ -40,5 +29,14 @@ function LibTranslationLoaderFactory() {
   ],
   declarations: [StatsPageComponent],
   exports: [StatsPageComponent],
+  providers: [
+    {
+      provide: TRANSLATIONS_TOKEN,
+      useValue: {
+        'en-US': en,
+        'pt-BR': pt,
+      },
+    },
+  ],
 })
 export class StatsShellModule {}
