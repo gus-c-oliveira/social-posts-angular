8560712d79a5baf5a3f1c5d593967569f53c6378
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-03-29 19:54:14 -0300

    e2e: add tests for user-list page

---
+++ apps/social-posts-angular-e2e/src/integration/user-list.spec.ts
@@ -0,0 +1,16 @@
+import { getUserCards, getUserProfile } from '../support/user-list.po';
+
+describe('user-list', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display the user cards', () => {
+    getUserCards().should('exist');
+    getUserCards().should('have.length', 10);
+  });
+
+  it('should navigate to user profile after clicking a card', () => {
+    getUserCards().first().click();
+    cy.location('pathname').should('match', /\/profile$/);
+    getUserProfile().should('exist');
+  });
+});
