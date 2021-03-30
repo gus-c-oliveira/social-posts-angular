188aa8427a7782a619b03fd7e7b9e71b18b8a894
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-03-30 08:18:57 -0300

    e2e: add tests for user profile page

---
+++ apps/social-posts-angular-e2e/src/support/user-profile.po.ts
@@ -0,0 +1,7 @@
+export const getUserName = () => cy.get('.cover__username');
+export const getAboutMeSection = () => cy.get('.general__container');
+export const getUserFriends = () => cy.get('.friends__item');
+export const getUserAddress = () => cy.get('.address');
+export const getUserCompany = () => cy.get('.company');
+export const getUserPostItems = () => cy.get('.post__item');
+export const getUserPost = () => cy.get('gus-user-post');
