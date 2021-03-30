988459656a1e336b4eee3ed855054dff7fec2b9d
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-03-30 08:30:23 -0300

    e2e: add tests foruser post

---
+++ apps/social-posts-angular-e2e/src/support/user-post.po.ts
@@ -0,0 +1,4 @@
+export const getPostImage = () => cy.get('.post__image');
+export const getPostTitle = () => cy.get('.post__title');
+export const getPostBody = () => cy.get('.post__body');
+export const getCommentItems = () => cy.get('.comment__item');
