4782dca0a88c811ea174e4f919bb61b6f49fdb0d
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-03 00:07:20 -0300

    comment-store: update Selectors

--- libs/comment-store/src/lib/selectors/comment.selectors.ts
+++ libs/comment-store/src/lib/selectors/comment.selectors.ts
@@ -1,7 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-import { Comment } from '../model/index';
-import { COMMENT_STATE_KEY, CommentState } from '../state/index';
+import { COMMENT_STATE_KEY, CommentState, adapter } from '../state/index';
 
 const getCommentState = createFeatureSelector<CommentState>(COMMENT_STATE_KEY);
 
@@ -15,10 +14,9 @@ const getError = createSelector(
   (state: CommentState): boolean => !!state.error
 );
 
-const getComments = createSelector(
-  getCommentState,
-  (state: CommentState): Comment[] => state.comments || []
-);
+const adapterSelectors = adapter.getSelectors();
+
+const getComments = createSelector(getCommentState, adapterSelectors.selectAll);
 
 export const commentQuery = {
   getLoading,
