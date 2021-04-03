93092a0c93360c5ff52f27b6b71d7abf6e2f8c3d
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-02 23:05:49 -0300

    post-store: implement PostState as an EntityState

--- libs/post-store/src/lib/state/post.state.ts
+++ libs/post-store/src/lib/state/post.state.ts
@@ -1,17 +1,18 @@
 import { Post } from '../model/index';
+import { EntityState, createEntityAdapter } from '@ngrx/entity';
 
 export const POST_STATE_KEY = 'post';
 
-export interface PostState {
-  posts: Post[];
-  loading: boolean;
+export interface PostState extends EntityState<Post> {
   error: boolean;
+  loading: boolean;
   selectedPostID: number;
 }
 
-export const initialPostState: PostState = {
-  posts: [],
+export const adapter = createEntityAdapter<Post>();
+
+export const initialPostState: PostState = adapter.getInitialState({
   loading: false,
   error: false,
   selectedPostID: null,
-};
+});
