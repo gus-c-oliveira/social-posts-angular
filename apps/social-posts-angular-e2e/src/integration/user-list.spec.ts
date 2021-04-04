a1ee1dd12e5066d1448d1ca94597ed2be8dfc913
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-04 09:13:05 -0300

    e2e: mock http request in user-list spec

--- apps/social-posts-angular-e2e/src/integration/user-list.spec.ts
+++ apps/social-posts-angular-e2e/src/integration/user-list.spec.ts
@@ -1,10 +1,17 @@
 import { getUserCards, getUserProfile } from '../support/user-list.po';
 
-describe('user-list', () => {
-  beforeEach(() => cy.visit('/'));
+describe.only('user-list', () => {
+  beforeEach(() => {
+    cy.fixture('user-list.json').as('userListJSON');
+    cy.server();
+    cy.route('https://jsonplaceholder.typicode.com/users', '@userListJSON').as(
+      'userList'
+    );
+    cy.visit('/');
+  });
 
   it('should display the user cards', () => {
-    getUserCards().should('exist').and('have.length', 10);
+    getUserCards().should('exist').and('have.length', 2);
   });
 
   it('should navigate to user profile after clicking a card', () => {
