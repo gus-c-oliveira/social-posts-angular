5665264a24f02a3c6f686612ed03495f5d026173
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-07 23:14:07 -0300

    comment-store: remove lib

--- libs/comment-store/README.md
+++
@@ -1,7 +0,0 @@
-# comment-store
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test comment-store` to execute the unit tests.
--- libs/comment-store/src/lib/token.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const COMMENT_SERVICE_BASE_URL = new InjectionToken<string>(
-  'Base URL for CommentService Requests'
-);
