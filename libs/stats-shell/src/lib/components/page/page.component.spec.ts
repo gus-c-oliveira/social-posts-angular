aaf3b6939b5a88f886d8437f05baefbb9ae9a83f
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-09 20:09:28 -0300

    stats-shell: update test setup

--- libs/stats-shell/src/lib/components/page/page.component.spec.ts
+++ libs/stats-shell/src/lib/components/page/page.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LanguageModule } from '@gus/language';
 import { getElementTextContentByDataTest } from '@gus/testing';
 import { StoreModule } from '@ngrx/store';
@@ -10,16 +10,18 @@ describe('StatsPageComponent', () => {
   let component: StatsPageComponent;
   let fixture: ComponentFixture<StatsPageComponent>;
 
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      imports: [
-        TranslateModule.forRoot(),
-        StoreModule.forRoot({}),
-        LanguageModule,
-      ],
-      declarations: [StatsPageComponent],
-    }).compileComponents();
-  });
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          TranslateModule.forRoot(),
+          StoreModule.forRoot({}),
+          LanguageModule,
+        ],
+        declarations: [StatsPageComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StatsPageComponent);
