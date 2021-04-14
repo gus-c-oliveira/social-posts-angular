07a3d2477e20be538f13e21e236af4c6cb2562a2
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-13 21:13:38 -0300

    user-store: add return type to addImageToPost

--- libs/user-store/src/lib/utils/post.ts
+++ libs/user-store/src/lib/utils/post.ts
@@ -1,6 +1,6 @@
 import { Post } from '../model';
 
-export const addImageToPost = (posts: Post[]) => {
+export const addImageToPost = (posts: Post[]): Post[] => {
   return posts.map((post) => ({
     ...post,
     img: `https://picsum.photos/seed/${post.id}/500`,
