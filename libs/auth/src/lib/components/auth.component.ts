13b4ef0732239ad1126d5bb5da89f38b445ac52a
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2021-04-18 11:16:24 -0300

    auth: add data-test to auth component

--- libs/auth/src/lib/components/auth.component.ts
+++ libs/auth/src/lib/components/auth.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../service/index';
 
+export const selector = 'gus-auth';
+
 @Component({
-  selector: 'gus-auth',
+  selector,
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
+  @HostBinding('attr.data-test') readonly dataTest = selector;
+
   public constructor(
     private authService: AuthService,
     private router: Router
